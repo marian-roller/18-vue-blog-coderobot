c5b4a5fe406cc2bfcd80e20d37fb70ecb82c80a6
Marian <webmaster@rollercoastery.pl>
2019-06-12 23:06:25 +0200

    vue setup with router

---
+++ src/components/Users.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="users">
+    
+      Users
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
