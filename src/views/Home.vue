9b5c28784dbee49ed9f3ab1522063370c25a34a0
Marian <webmaster@rollercoastery.pl>
2019-06-13 22:57:42 +0200

    navbar and get users initial success

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <h1>Home</h1>
+
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  //import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    //HelloWorld
+  export default {
+    name: 'home',
+    components: {
+      //HelloWorld
+    }
   }
-}
 </script>
