e7444da03568cffebd895dfa1c7e4a4ebc97de0e
Marian <webmaster@rollercoastery.pl>
2019-06-23 11:09:15 +0200

    display categories on single post view

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="container">
         <h1>{{ post.title }}</h1>
-
+        <p class="mb-0"><small>Utworzony: <strong>{{ post.created_at }}</strong></small></p>
+        <p class="mb-0"><small>Zmodyfikowany: <strong>{{ post.updated_at }}</strong></small></p>
+        <p>
+            <small>Kategorie:
+                <span class="bg-light mx-2 border px-1 rounded-sm" v-for="category in post.categories">
+                    <strong>{{category}}</strong>
+                </span>
+            </small>
+        </p>
+        <p> {{ post.body }}</p>
 
     </div>
 </template>
